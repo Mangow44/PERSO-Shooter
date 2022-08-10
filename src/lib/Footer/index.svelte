512b048b76457635c29de77193071294d20f35f8
Thomas Clouet <thomas.clouet1@etu.univ-nantes.fr>
2022-08-10 20:35:04 +0200

    :zap: Ammunition UX

--- src/lib/Footer/index.svelte
+++ src/lib/Footer/index.svelte
@@ -1,14 +1,29 @@
 <script>
 	export let player = { ammunition: 0 };
 
-	$: ammunition = player.ammunition;
+	let ammunition = [];
+
+	$: if (player) {
+		ammunition = [];
+		let nbTrue = player.ammunition;
+		let nbFalse = 10 - player.ammunition;
+
+		for (let i = 0; i < nbTrue; i++) {
+			ammunition = [...ammunition, true];
+		}
+		for (let i = 0; i < nbFalse; i++) {
+			ammunition = [...ammunition, false];
+		}
+	}
 </script>
 
 <div
 	class="flex h-[3rem] w-full
 		text-c-white bg-c-dark-gray"
 >
-	<div class="flex h-full w-[10rem] mx-auto">
-		<p class="m-auto">ammunition : {ammunition}</p>
+	<div class="flex m-auto">
+		{#each ammunition as ammo}
+			<span class="h-4 w-4 transition-colors {ammo ? 'bg-white' : 'bg-gray-500'} " />
+		{/each}
 	</div>
 </div>
