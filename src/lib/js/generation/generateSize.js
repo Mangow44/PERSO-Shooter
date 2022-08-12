88bfd637f620fb3fe2299d1986d71ce15e6cfe72
Thomas Clouet <thomasclouet@Jans-iMac.local>
2022-08-12 09:51:13 +0200

    :art: Ratio for big screens

--- src/lib/js/generation/generateSize.js
+++ src/lib/js/generation/generateSize.js
@@ -3,12 +3,18 @@ export const generateSize = () => {
 	let screenSize = window.innerHeight * window.innerWidth;
 	let ratio = 1;
 
-	if (screenSize > 55000) {
+	if (screenSize >= 55000 && screenSize < 1000000) {
 		ratio = 1.3;
+	}
+	if (screenSize >= 1000000) {
+		ratio = 1.6;
 	}
 
 	let height = size * ratio;
 	let width = size * ratio;
 
+	console.log(screenSize);
+
+	console.log(ratio);
 	return { height, width };
 };
